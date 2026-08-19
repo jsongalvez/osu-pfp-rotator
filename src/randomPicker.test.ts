1d4df3d24b41e3ad16130c83a1845023b1d2fcd8
Jayson Galvez <jaysonsullanogalvez@gmail.com>
2026-08-19 22:34:53 +0800

    fix: sharpen test names

--- src/randomPicker.test.ts
+++ src/randomPicker.test.ts
@@ -5,10 +5,10 @@ const imgA = "https://a.s-ul.eu/x.png";
 const imgB = "https://b.s-ul.eu/y.png";
 const imgC = "https://c.s-ul.eu/z.png";
 
-test("randomPicker selects last image", () => {
-  expect(
-    randomPicker([imgA, imgB, imgC], imgB, { random: () => 0.99 }),
-  ).toBeOneOf([imgA, imgC]);
+test("randomPicker selects final candidate", () => {
+  expect(randomPicker([imgA, imgB, imgC], imgB, { random: () => 0.99 })).toBe(
+    imgC,
+  );
 });
 
 test("randomPicker returns image for one entry if not same entry", () => {
